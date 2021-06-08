313a058d527e4445aa9e733fa4593c9be533bf4c
Faizal Surya <faizalsurya15@gmail.com>
2021-06-08 15:22:10 +0700

    Done :)

--- app/src/androidTest/java/com/cals/movies/MenuActivityTest.kt
+++ app/src/androidTest/java/com/cals/movies/MenuActivityTest.kt
@@ -4,6 +4,7 @@ package com.cals.movies
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
@@ -37,8 +38,6 @@ class MenuActivityTest{
         IdlingRegistry.getInstance().unregister(EspressoIdlingResource.idlingResource)
     }
 
-
-
     @Test
     fun loadMovies(){
         onView(withId(R.id.rv_movie)).check(matches(isDisplayed()))
@@ -57,7 +56,37 @@ class MenuActivityTest{
         onView(withId(R.id.tv_item_overview)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_item_overview)).check(matches(withText(staticMovie[0].description)))
         onView(withId(R.id.img_detail)).check(matches(isDisplayed()))
+        Espresso.pressBack()
+    }
 
+    @Test
+    fun loadtvshow(){
+        onView(withId(R.id.navigation_dashboard)).perform(click())
+        onView(withId(R.id.rv_tv_show)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_tv_show)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(staticMovie.size))
+    }
+
+    @Test
+    fun loadDetailtvshow(){
+        onView(withId(R.id.navigation_dashboard)).perform(click())
+        onView(withId(R.id.rv_tv_show)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_tv_show)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.active_favorite)).perform(click())
+        onView(withId(R.id.tv_item_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_item_title)).check(matches(withText(staticTvShow[0].title)))
+        onView(withId(R.id.tv_item_release)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_item_release)).check(matches(withText(staticTvShow[0].release_date)))
+        onView(withId(R.id.tv_item_overview)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_item_overview)).check(matches(withText(staticTvShow[0].description)))
+        onView(withId(R.id.img_detail)).check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun loadFavorite(){
+        onView(withId(R.id.navigation_notifications)).perform(click())
+        onView(withId(R.id.rv_movie)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(staticMovie.size))
     }
 
 }
